974302f521a3fb995f4a1dee1b8d674e8ffe33ac
YUE LI <641847518@qq.com>
2023-08-02 14:55:04 -0500

    渲染一级导航列表

--- src/main.js
+++ src/main.js
@@ -7,10 +7,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import { getCategory } from './apis/testAPI'
-getCategory().then(res => {
-    console.log(res)
-})
 
 const app = createApp(App)
 
