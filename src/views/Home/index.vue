49d5873045b732bc763aa0416227ad2003cb1194
YUE LI <641847518@qq.com>
2023-08-03 01:49:59 -0500

    home--整体拆分及分类数据渲染

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,4 +1,20 @@
+<script setup>
+
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import HomeProduct from './components/HomeProduct.vue'
+
+
+</script>
+
 <template>
-    I'm home page
-    <div style="height: 1200px;"></div>
+    <div class="container">
+        <HomeCategory />
+        <HomeBanner />
+    </div>
+    <HomeNew />
+    <HomeHot />
+    <HomeProduct />
 </template>
