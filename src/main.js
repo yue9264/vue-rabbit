a1ba325a99075045adfbae1e2dd605c063dd0903
YUE LI <641847518@qq.com>
2023-08-03 23:46:25 -0500

    懒加载

--- src/main.js
+++ src/main.js
@@ -7,11 +7,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { useIntersectionObserver } from '@vueuse/core'
 
+//引入懒加载指令插件并注册
+import { lazyPlugin } from '@/directives'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
+
+
