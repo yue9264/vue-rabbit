d578c0a838db79cbcfff43c24267980b3c905c32
YUE LI <641847518@qq.com>
2023-08-05 17:53:53 -0500

    解决路由缓存问题

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -4,14 +4,20 @@ import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import { getBannerAPI  } from "@/apis/home";
 import GoodsItem from "../Home/components/GoodsItem.vue";
+import { onBeforeRouteUpdate } from 'vue-router';
+
 
 const categoryData = ref({})
 const route = useRoute()
-const getCategory = async() => {
-    const res = await getCategoryAPI(route.params.id)
+const getCategory = async(id=route.params.id) => {
+    const res = await getCategoryAPI(id)
     categoryData.value = res.result
 }
 onMounted(() => getCategory())
+
+onBeforeRouteUpdate((to) => {
+  getCategory(to.params.id)
+})
 
 //获取banner
 
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -7,17 +7,18 @@ import LayoutFixed from './components/LayoutFixed.vue';
 import { useCategoryStore } from '@/stores/category'
 import { onMounted } from 'vue'
 
+
 const categoryStore = useCategoryStore()
 
 onMounted(() => categoryStore.getCategory())
-
-
 </script>
 
 <template>
     <LayoutFixed />
     <LayoutNav />
     <LayoutHeader />
+    <!--添加key，破坏复用机制，强制销毁重建-->
+    <!--<RouterView :key="$route.fullPath" />-->
     <RouterView />
     <LayoutFooter />
 </template>
