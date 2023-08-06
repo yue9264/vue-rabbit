bc2d8df1cec0961787ff486a2f4e8e07c288c5d9
YUE LI <641847518@qq.com>
2023-08-06 16:00:17 -0500

    列表筛选功能实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -28,6 +28,11 @@ const getGoodList = async() => {
 }
 onMounted(()=> getGoodList())
 
+//tab切换回调
+const tabChange = () => {
+  reqData.value.page = 1
+  getGoodList()
+}
 
 </script>
 <!-- `/category/${categoryData.parentId}` -->
@@ -43,7 +48,7 @@ onMounted(()=> getGoodList())
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
