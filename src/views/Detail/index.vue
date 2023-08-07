564c5880856ce0f111a292f06543529d1a282c7b
YUE LI <641847518@qq.com>
2023-08-07 15:04:16 -0500

    详情-热榜-基础组件封装和渲染

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -2,6 +2,7 @@
 import { getDetail } from '@/apis/detail'
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
+import DetailHot from './components/DetailHot.vue'
 
 const goods = ref({})
 const route = useRoute()
@@ -114,7 +115,10 @@ onMounted(() => getGoods())
             </div>
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
-
+              <!-- 24小时 -->
+              <DetailHot/>
+              <!-- 周 -->
+              <DetailHot/>
             </div>
           </div>
         </div>
