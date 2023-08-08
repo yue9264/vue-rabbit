4f19ad00482e0ff3abef1e285695ea52dd695155
YUE LI <641847518@qq.com>
2023-08-08 18:45:33 -0500

    详情页--sku组件

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -4,6 +4,7 @@ import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import DetailHot from './components/DetailHot.vue'
 import ImageView from '@/components/ImageView/index.vue'
+import XtxSku from '@/components/XtxSku/index.vue'
 
 const goods = ref({})
 const route = useRoute()
@@ -13,6 +14,10 @@ const getGoods = async() =>{
 }
 onMounted(() => getGoods())
 
+//sku规格被操作时
+const skuChange = (sku) => {
+  console.log(sku)
+}
 
 </script>
 
@@ -82,7 +87,7 @@ onMounted(() => getGoods())
                 </dl>
               </div>
               <!-- sku组件 -->
-
+              <XtxSku :goods="goods" @change="skuChange"/>
               <!-- 数据组件 -->
 
               <!-- 按钮组件 -->
