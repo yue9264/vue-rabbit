24f26830caadf632766873420d4d8f1d70870e54
YUE LI <641847518@qq.com>
2023-08-10 23:40:24 -0500

    token失效401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,8 +1,8 @@
 //axios基础封装
 import axios from "axios";
 import { ElMessage } from 'element-plus';
-import 'element-plus/theme-chalk/el-message.css'
 import { useUserStore } from "@/stores/user";
+import router from "@/router";
 
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -17,7 +17,7 @@ httpInstance.interceptors.request.use(config =>{
     const userStore = useUserStore()
     const token = userStore.userInfo.token
     if (token){
-        config.headers.Authorization = `bearer ${token}`
+        config.headers.Authorization = `Bearer ${token}`
     }
     return config
 }, e => Promise.reject(e))
@@ -25,10 +25,15 @@ httpInstance.interceptors.request.use(config =>{
 //axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
     //统一错误提示
+    const userStore = useUserStore()
     ElMessage({
         type: 'warning',
         message:e.response.data.message
     })
+    if (e.response.status === 401){
+        userStore.clearUserInfo()
+        router.push('/login')
+    }
     return Promise.reject(e)
 })
 
