06812f6fd88b35bb956e3dc82fdf24bcd26cf0bc
YUE LI <641847518@qq.com>
2023-08-13 16:08:23 -0500

    购物车--头部购物车删除功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -22,9 +22,16 @@ export const useCartStore = defineStore('cart', () => {
         }
         
     }
+
+    const delCart = (skuId) => {
+        const idx = cartList.value.findIndex((item) => skuId ===item.skuId)
+        cartList.value.splice(idx,1)
+    }
+
     return {
         cartList,
-        addCart
+        addCart,
+        delCart
     }
 
 }, {
