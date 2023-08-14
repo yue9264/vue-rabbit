ce547fe10df3ce122f7dc61cf9e7312d7a97dcfc
YUE LI <641847518@qq.com>
2023-08-14 16:49:48 -0500

    购物车列表统计数据实现

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -45,7 +45,10 @@ export const useCartStore = defineStore('cart', () => {
     //2.总价 所有项的count*price之和
     const allPrice = computed(()=> cartList.value.reduce((a,c) => a + c.count*c.price, 0))
 
-
+    //3.已选择数量 所有项的count之和
+    const selectedCount = computed(() => cartList.value.filter(item => item.selected).reduce((a,c) => a+c.count,0))
+    //4.已选商品价格合计
+    const selectedPrice = computed(() => cartList.value.filter(item => item.selected).reduce((a,c) => a+c.count * c.price,0))
     //是否全选
     const isAll = computed(() => cartList.value.every((item) => item.selected))
 
@@ -57,7 +60,9 @@ export const useCartStore = defineStore('cart', () => {
         allPrice,
         singleCheck,
         isAll,
-        allCheck
+        allCheck,
+        selectedCount,
+        selectedPrice
     }
 
 }, {
